d30247113eb7e3fedd68f7f1ad91d3fdeefc205c
GentianVeliu <genti@GentianVelius-iMac.local>
2020-07-09 13:31:41 +0200

    Initial Commit

---
+++ ProjektiNeIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjektiNeIOS
+//
+//  Created by GentianVeliu on 7/9/20.
+//  Copyright © 2020 GentianVeliu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
